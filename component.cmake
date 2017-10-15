465d66019996f1305be8ab6ceae8ae426c9cf800
David Callu <callu.david@gmail.com>
2017-10-15 19:32:13 +0200

    feat(cmake): add data to build version 58.2

---
+++ component.cmake
@@ -0,0 +1,12 @@
+include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
+
+
+
+byd__package__add_component(${package}
+    extra
+    icuio
+    layoutex
+    tools
+    tests
+    samples
+    )
---
+++ id.cmake
@@ -0,0 +1,14 @@
+set(package icu)
+
+byd__package__info(${package}
+    MAINTAINER_NAME "David Callu"
+    MAINTAINER_EMAIL "callu.david@gmail.com"
+    VERSION 58.2
+    PACKAGE_VERSION 0.1
+    ABI 58.2
+    )
+
+byd__package__download_info(
+    URL "http://download.icu-project.org/files/icu4c/58.2/icu4c-58_2-src.tgz"
+    URL_MD5 fac212b32b7ec7ab007a12dff1f3aea1
+    )
