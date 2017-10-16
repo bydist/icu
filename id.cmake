48c50f498cf28ac09c356f44dd7ee4d91eecfbea
David Callu <callu.david@gmail.com>
2017-10-16 09:07:18 +0200

    fix(id): add missing argument to byd__package__download_info

--- id.cmake
+++ id.cmake
@@ -8,7 +8,7 @@ byd__package__info(${package}
     ABI 58.2
     )
 
-byd__package__download_info(
+byd__package__download_info(${package}
     URL "http://download.icu-project.org/files/icu4c/58.2/icu4c-58_2-src.tgz"
     URL_MD5 fac212b32b7ec7ab007a12dff1f3aea1
     )
