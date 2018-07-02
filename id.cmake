4cdb8d824e4dd19ab1ac9fd2a2e62de0b171ffc1
David Callu <callu.david@gmail.com>
2018-07-02 11:14:05 +0200

    feat(version): update to 62.1

--- id.cmake
+++ id.cmake
@@ -1,10 +1,10 @@
 set(package icu)
-set(version 61.1)
+set(version 62.1)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-3
+    VERSION ${version}-1
     ABI ${version}
     )
 
@@ -13,5 +13,5 @@ string(REGEX REPLACE "\\\." "_" version_underscore ${version})
 
 byd__package__download_info(${package}
     URL "http://download.icu-project.org/files/icu4c/${version}/icu4c-${version_underscore}-src.tgz"
-    HASH SHA1=06ca7b1e64c28e07d5633a2e0257380884ea486b
+    HASH SHA1=1884f26568cd773b984580b4fca422a3e2afe35d
     )
