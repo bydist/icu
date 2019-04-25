69b8cd34b9ab74b05ebb48b4d51966be7c53136e
David Callu <callu.david@gmail.com>
2019-04-25 12:29:13 +0200

    chore(id): bump to 64.2-1

--- id.cmake
+++ id.cmake
@@ -1,10 +1,10 @@
 set(package icu)
-set(version 62.1)
+set(version 64.2)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-2
+    VERSION ${version}-1
     ABI ${version}
     )
 
@@ -13,5 +13,5 @@ string(REGEX REPLACE "\\\." "_" version_underscore ${version})
 
 byd__package__download_info(${package}
     URL "http://download.icu-project.org/files/icu4c/${version}/icu4c-${version_underscore}-src.tgz"
-    HASH SHA1=1884f26568cd773b984580b4fca422a3e2afe35d
+    HASH MD5=a3d18213beec454e3cdec9a3116d6b05
     )
